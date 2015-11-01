0ec8ab0c33cd53b802560c2b4b540e9f28cfd493
Jose Manuel <jmanuel_1407@hotmail.com>
2015-10-31 18:14:48 -0600

    despliegue nube

--- models/models.js
+++ models/models.js
@@ -3,13 +3,13 @@ var path = require ('path');
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite   DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-
-var DB_name  = (url[6]||null);
-var user     = (url[2]||null);
-var pwd      = (url[3]||null);
+//postgres://aunxushphknmkz:IJbEh_qdfK5VeZrC0rtB6AVs3R@ec2-107-21-221-59.compute-1.amazonaws.com:5432/d2olh5b5q7p338
+var DB_name  = ('d2olh5b5q7p338'||null);
+var user     = ('aunxushphknmkz'||null);
+var pwd      = ('IJbEh_qdfK5VeZrC0rtB6AVs3R'||null);
 var protocol = (url[1]||null);
 var dialect  = (url[1]||null);
-var port     = (url[5]||null);
+var port     = ('5432'||null);
 var host     = (url[4]||null);
 var storage  = process.env.DATABASE_STORAGE;
 
@@ -27,16 +27,26 @@ var sequelize = new Sequelize(DB_name, user, pwd,
   }      
 );
 
+//Usar BBDD SQLite
+/*
+var sequelize = new Sequelize(null,null,null,
+	{
+		dialect: "sqlite", storage:"usuarios.sqlite"
+	});
+*/
+
+
 //importar la definición de la tabla usuarios en usuarios.js
 
-var Usuarios = sequelize.import(path.join(__dirname,'usuarios'));
+var usuarios_path = path.join(__dirname,'usuarios');
+var Usuarios = sequelize.import(usuarios_path);
 
 exports.Usuarios = Usuarios;
 
 //sequelize.sync() crea e inicializa la tabla de usuarios en la BD
-sequelize.sync().success(function(){
+sequelize.sync().then(function(){
 	//succes(...) ejecuta el manejador una vez creada la tabla
-	Usuarios.count().success(function(count){
+	Usuarios.count().then(function(count){
 		if(count==0){//La tabla se inicializa sólo si esta vacía
 			Usuarios.create({
 				usuario:'admin',
@@ -45,7 +55,7 @@ sequelize.sync().success(function(){
 				edad:'22',
 				sexo:'hombre',
 				peso:'68'
-			}).success(function(){console.log('Base de datos inicializada')});
+			}).then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
