c0b9e5691357f04fc7a5192170405b6957b37d87
Jannet24 <letty_217@hotmail.com>
2015-12-18 14:20:30 -0500

    heroku

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ app.set('view engine', 'ejs');
 
 app.use(partials());
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser('Quiz 2015')); //Para cifrar cookie
+app.use(cookieParser('e-nutrition2015')); //Para cifrar cookie
 app.use(session());
 app.use(express.static(path.join(__dirname, 'public')));
 
